f3cb29989a61f6a26cdb7342efecd5ba90a7dc5e
rasoaral <107067184+rasoaral@users.noreply.github.com>
2022-06-07 11:46:43 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,120 @@
+/*desvaneser*/
+window.sr=ScrollReveal();
+sr.reveal("#texto2", {
+    duration: 3500,
+    origin: "botton",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#texto3", {
+    duration: 3000,
+    origin: "left",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#lineas3", {
+    duration: 2000,
+    origin: "right",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#lineas4", {
+    duration: 2000,
+    origin: "left",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#prifoto", {
+    duration: 2000,
+});
+window.sr=ScrollReveal();
+sr.reveal("#lineas5", {
+    duration: 2000,
+    origin: "right",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#lineas6", {
+    duration: 2000,
+    origin: "left",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#segfoto", {
+    duration: 2000,
+});
+window.sr=ScrollReveal();
+sr.reveal("#cons3", {
+    origin: "top",
+    duration: 2000,
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#cons", {
+    origin: "top",
+    duration: 2000,
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#lineas8", {
+    duration: 2000,
+    origin: "right",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#lineas7", {
+    duration: 2000,
+    origin: "left",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#terfoto", {
+    duration: 2000,
+});
+window.sr=ScrollReveal();
+sr.reveal("#cons4", {
+    origin: "top",
+    duration: 2000,
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#lineas9", {
+    duration: 2000,
+    origin: "right",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#lineas10", {
+    duration: 2000,
+    origin: "left",
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#cuafoto", {
+    duration: 2000,
+});
+window.sr=ScrollReveal();
+sr.reveal("#cons5", {
+    origin: "top",
+    duration: 2000,
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#insta2", {
+    origin: "top",
+    duration: 2000,
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#face2", {
+    origin: "top",
+    duration: 2000,
+    distance: "-100px"
+});
+window.sr=ScrollReveal();
+sr.reveal("#siu", {
+    origin: "left",
+    origin: "top",
+    duration: 2000,
+    distance: "1000px"
+});
